f7a84a4f6264617fa7c0a2d69ffb7d5f399f83bd
Gust87 <gustiferreyra_87@hotmail.com>
2018-08-28 23:31:04 -0300

    ButtonScript Completo
    Solo el traslado de escenas (Menú Principal, Juego y Opciones)

---
+++ Assets/Scrips/ButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour {
+
+    public void Exit()
+    {
+        Exit();
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
