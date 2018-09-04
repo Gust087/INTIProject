3ea018298ae3688e85a61f5b1bb93ed72a521980
Alejandro Zbrun <ajzbrun@gmail.com>
2018-09-04 19:20:59 -0300

    ale
    asd

---
+++ Assets/Scrips/ButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+
+    public SpriteRenderer popUp;
+    private void Start()
+    {
+        popUp = GetComponent<SpriteRenderer>();
+    }
+    public void Exit()
+    {
+        Exit();
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void HelpMain()
+    {
+        popUp.sortingOrder = 5;
+    }
+}
