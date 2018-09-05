5c6e280d51e76c036e5bd09a462ace854f2f39f3
Gust87 <gustiferreyra_87@hotmail.com>
2018-09-05 18:18:05 -0300

    Arreglo de estanterías

--- Assets/Scrips/WorkerScript.cs
+++ Assets/Scrips/WorkerScript.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class WorkerScript : MonoBehaviour {
 
+    public Animator animator;
     public int cantMovement;
+
     int count = 0;
     int numberDir = 0;
- //   int[] directions = new int[] {'E','O','S','N' };
+
+    const string
+        wRight = "right",
+        wLeft = "left",
+        wUp = "up",
+        wDown = "down"
+        ;
+    //   int[] directions = new int[] {'E','O','S','N' };
 
     // Use this for initialization
     void Start () {
@@ -15,6 +24,9 @@ public class WorkerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        CheckAnims(Move(numberDir));
+
         if (count > cantMovement) {
             numberDir++;
             count = 0;
@@ -43,4 +55,53 @@ public class WorkerScript : MonoBehaviour {
                 return new Vector2(0f,0f);
         }
     }
+
+    void CheckAnims(Vector2 v)
+    {
+        if (animator == null)
+        {
+            return;
+        }
+        if (v[0] < 0)
+        {
+            animator.SetBool(wRight, false);
+            animator.SetBool(wUp, false);
+            animator.SetBool(wDown, false);
+            animator.SetBool(wLeft, true);
+
+        }
+        else if(v[0] > 0)
+        {
+            animator.SetBool(wLeft, false);
+            animator.SetBool(wUp, false);
+            animator.SetBool(wDown, false);
+            animator.SetBool(wRight, true);
+
+        }
+        else if (v[1] < 0)
+        {
+            animator.SetBool(wRight, false);
+            animator.SetBool(wLeft, false);
+            animator.SetBool(wUp, false);
+            animator.SetBool(wDown, true);
+
+        }
+        else if (v[1] > 0)
+        {
+            animator.SetBool(wRight, false);
+            animator.SetBool(wLeft, false);
+            animator.SetBool(wDown, false);
+            animator.SetBool(wUp, true);
+        }
+        else
+        {
+            animator.SetBool(wRight, false);
+            animator.SetBool(wLeft, false);
+            animator.SetBool(wUp, false);
+            animator.SetBool(wDown, false);
+
+        }
+
+    }
+
 }
